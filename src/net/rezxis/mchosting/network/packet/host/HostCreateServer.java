b8e18798b9574590c7723cac9e0deaddd8d9c595
cur <cur@DESKTOP-26QAR5C>
2019-12-23 09:15:29 +0900

    Custom追加用

--- src/net/rezxis/mchosting/network/packet/host/HostCreateServer.java
+++ src/net/rezxis/mchosting/network/packet/host/HostCreateServer.java
@@ -9,11 +9,13 @@ public class HostCreateServer extends Packet {
 	public String player;
 	public String displayName;
 	public String world;
+	public String stype;
 	
-	public HostCreateServer(String player, String displayName, String enm) {
+	public HostCreateServer(String player, String displayName, String enm, String stype) {
 		super(PacketType.CreateServer, ServerType.HOST);
 		this.player = player;
 		this.displayName = displayName;
 		this.world = enm;
+		this.stype = stype;
 	}
 }
--- src/net/rezxis/mchosting/network/packet/sync/SyncCreateServer.java
+++ src/net/rezxis/mchosting/network/packet/sync/SyncCreateServer.java
@@ -9,11 +9,13 @@ public class SyncCreateServer extends Packet {
 	public String player;
 	public String displayName;
 	public String world;
+	public String stype;
 	
-	public SyncCreateServer(String player, String displayName, String enm) {
+	public SyncCreateServer(String player, String displayName, String enm, String stype) {
 		super(PacketType.CreateServer, ServerType.SYNC);
 		this.player = player;
 		this.displayName = displayName;
 		this.world = enm;
+		this.stype = stype;
 	}
 }
