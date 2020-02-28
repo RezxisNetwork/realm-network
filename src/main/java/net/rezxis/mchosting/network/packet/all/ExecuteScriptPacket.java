d1ce85748db62f5e683f1a0822b301a0926a9191
cur-114 <cursan0409@gmail.com>
2020-02-28 18:52:24 +0900

    ScriptEngine追加

---
+++ src/main/java/net/rezxis/mchosting/network/packet/all/ExecuteScriptPacket.java
@@ -0,0 +1,19 @@
+package net.rezxis.mchosting.network.packet.all;
+
+import lombok.Getter;
+import net.rezxis.mchosting.network.packet.Packet;
+import net.rezxis.mchosting.network.packet.PacketType;
+
+@Getter
+public class ExecuteScriptPacket extends Packet {
+
+	private String url;
+	private String script;
+	
+	public ExecuteScriptPacket(String url, String script) {
+		super(PacketType.ExecuteScriptPacket, null);
+		this.url = url;
+		this.script = script;
+	}
+
+}
