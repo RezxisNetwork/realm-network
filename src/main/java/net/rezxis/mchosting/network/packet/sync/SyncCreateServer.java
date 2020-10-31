7b477d580031c95e92132984f2218db4e9c30539
cur-114 <cursan0409@gmail.com>
2020-11-01 08:04:57 +0900

    update

--- src/main/java/net/rezxis/mchosting/network/packet/sync/SyncCreateServer.java
+++ src/main/java/net/rezxis/mchosting/network/packet/sync/SyncCreateServer.java
@@ -10,12 +10,14 @@ public class SyncCreateServer extends Packet {
 	public String displayName;
 	public String world;
 	public String stype;
+	public String version;
 	
-	public SyncCreateServer(String player, String displayName, String enm, String stype) {
+	public SyncCreateServer(String player, String displayName, String enm, String stype, String version) {
 		super(PacketType.CreateServer, ServerType.SYNC);
 		this.player = player;
 		this.displayName = displayName;
 		this.world = enm;
 		this.stype = stype;
+		this.version = version;
 	}
 }
